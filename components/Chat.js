1ab77e01b444cc6416a6fa7431e233f1904f3439
vskelton <vskelton25@gmail.com>
2025-03-29 17:29:58 -0400

    **

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,65 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
   const { name, color } = route.params;
+  const [messages, setMessages] = useState([]);
+
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
 
   useEffect(() => {
     navigation.setOptions({ title: name })
   }, []);
 
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: 'You have entered the chat',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
+  const renderBubble = (props) => {
+    return <Bubble
+    {...props}
+    wrapperStyle={{
+      right: {
+        backgroundColor: "#000"
+      },
+      left: {
+        backgroundColor: "#FFF"
+      }
+    }}
+    />
+  }
+
  return (
    <View style={[styles.container, { backgroundColor: color }]}>
-     <Text>Hello Screen2!</Text>
+     <GiftedChat 
+     messages={messages}
+     renderBubble={renderBubble}
+     onSend={messages => onSend(messages)}
+     user={{
+      _id: 1
+     }}
+     />
+     { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
    </View>
  );
 }
